05da679b06bdef4752758209b92f7a8a5f3b9e05
juanmagape <juanmagape.trb@gmail.com>
2026-02-25 08:26:47 +0100

    Cambiar clase

--- juego/src/Personaje.java
+++ juego/src/Personaje.java
@@ -1,10 +1,18 @@
 public class Personaje {
     private String nombre;
     private int skillPoints = 100;
+    private int cantMana;
+    private int danoCuerpoaCuerpo;
+    private int danoMagia;
+    private int vida;
 
-    public Personaje(String nombre, int skillPoints) {
+    public Personaje(String nombre, int skillPoints, int cantMana, int danoCuerpoaCuerpo, int danoMagia, int vida) {
         this.nombre = nombre;
         this.skillPoints = skillPoints;
+        this.cantMana = cantMana;
+        this.danoCuerpoaCuerpo = danoCuerpoaCuerpo;
+        this.danoMagia = danoMagia;
+        this.vida = vida;
     }
 
     public String getNombre() {
@@ -21,5 +29,47 @@ public class Personaje {
 
     public void setSkillPoints(int skillPoints) {
         this.skillPoints = skillPoints;
+    }
+
+    public int getCantMana() {
+        return cantMana;
+    }
+
+    public void setCantMana(int cantMana) {
+        this.cantMana = cantMana;
+    }
+
+    public int getDanoCuerpoaCuerpo() {
+        return danoCuerpoaCuerpo;
+    }
+
+    public void setDanoCuerpoaCuerpo(int danoCuerpoaCuerpo) {
+        this.danoCuerpoaCuerpo = danoCuerpoaCuerpo;
+    }
+
+    public int getDanoMagia() {
+        return danoMagia;
+    }
+
+    public void setDanoMagia(int danoMagia) {
+        this.danoMagia = danoMagia;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public void mostrarEstadisticasJug() {
+        System.out.println("=======================================================");
+        System.out.println("Nombre: " + this.getNombre());
+        System.out.println("Vida: " + this.vida);
+        System.out.println("Cantidad mana: " + this.cantMana);
+        System.out.println("Daño cuerpo a cuerpo: " + this.danoCuerpoaCuerpo);
+        System.out.println("Ataque magico: " + this.danoMagia);
+        System.out.println("=======================================================");
     }
 }
